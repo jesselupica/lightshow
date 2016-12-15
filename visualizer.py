4ec975bc7da353c73d43d8b20042d10562318d72
jesselupica <jesselupica@gmail.com>
2016-12-15 16:23:26 -0600

    works well. Bass not punchy?

--- visualizer.py
+++ visualizer.py
@@ -12,10 +12,12 @@ class ColorVisualizer:
     # GRAVITY = -4
 
     ACCELERATION = 12.5
-    RGB_CONST = 12, 20, 16
+    INIT_RGB_COEFF = 12, 20, 16
     GRAVITY = -6
-    MAX_INTENSITY = 255
-    MIN_INTENSITY = 0
+    RGB_INTENSITY_MAX = 255
+    RGB_INTENSITY_MIN = 0
+    RGB_INTENSITY_THRESHOLD = 200
+    RGB_COEFF_INCR = 1.02
     SIGNAL_INTENSITY_MAX_START = 300
     SIGNAL_INTENSITY_THRESHOLD = 1800
     
@@ -24,9 +26,9 @@ class ColorVisualizer:
 
     def __init__(self, r, g, b):
         r, g, b = self._bounds_check(r, g, b)
-        self.red = r if r > 1 else r * ColorVisualizer.MAX_INTENSITY
-        self.green = g if g < 1 else g * ColorVisualizer.MAX_INTENSITY
-        self.blue = b if b < 1 else b * ColorVisualizer.MAX_INTENSITY
+        self.red    = r if r > 1 else r * ColorVisualizer.RGB_INTENSITY_MAX
+        self.green  = g if g > 1 else g * ColorVisualizer.RGB_INTENSITY_MAX
+        self.blue   = b if b > 1 else b * ColorVisualizer.RGB_INTENSITY_MAX
 
         self.rv = 0
         self.bv = 0
@@ -38,6 +40,8 @@ class ColorVisualizer:
         # 3 for RGB 
         self.signal_intensity_maxes = [ColorVisualizer.SIGNAL_INTENSITY_MAX_START] * 3
 
+        self.rgb_history = [deque([0], maxlen=50)] * 3
+        self.rgb_max_mults = [1] * 3
     
 
     def tuple(self):
@@ -45,13 +49,13 @@ class ColorVisualizer:
         return (self.red, self.green, self.blue)
 
     def _bounds_check(self, r, g, b):
-        r = min(r, ColorVisualizer.MAX_INTENSITY)
-        g = min(g, ColorVisualizer.MAX_INTENSITY)
-        b = min(b, ColorVisualizer.MAX_INTENSITY)
+        r = min(r, ColorVisualizer.RGB_INTENSITY_MAX)
+        g = min(g, ColorVisualizer.RGB_INTENSITY_MAX)
+        b = min(b, ColorVisualizer.RGB_INTENSITY_MAX)
 
-        r = max(r, ColorVisualizer.MIN_INTENSITY)
-        g = max(g, ColorVisualizer.MIN_INTENSITY)
-        b = max(b, ColorVisualizer.MIN_INTENSITY)
+        r = max(r, ColorVisualizer.RGB_INTENSITY_MIN)
+        g = max(g, ColorVisualizer.RGB_INTENSITY_MIN)
+        b = max(b, ColorVisualizer.RGB_INTENSITY_MIN)
 
         return r, g, b
 
@@ -65,7 +69,7 @@ class ColorVisualizer:
         l = len(self.signal_history)
         return r_sum/l, g_sum/l, b_sum/l
 
-    def _choose_rgb_signals(self, freq_amps, dt):
+    def _choose_rgb_signals(self, freq_amps):
         # for i, m_i in enumerate(max_intensities):
         #    if m_i > intensity_drag:
         #        max_intensities[i] = max(300, m_i - intensity_drag)
@@ -77,44 +81,48 @@ class ColorVisualizer:
         #print([ int(2**((i-49)/12) * 440) for i, a in rgb])
         return [x[1] for x in rgb]
 
-
-
+    def _update_signal_intensity_multipliers(self):
+        avg_rgb = [sum(d)/len(d) for d in self.rgb_history]
+        #print(avg_rgb, len(self.rgb_history[0]))
+        for i, avg_c in enumerate(avg_rgb):
+            if avg_c > ColorVisualizer.RGB_INTENSITY_THRESHOLD:
+                self.rgb_max_mults[i] *= ColorVisualizer.RGB_COEFF_INCR
+            elif self.rgb_max_mults[i] > 1:
+                self.rgb_max_mults[i] /= ColorVisualizer.RGB_COEFF_INCR 
 
     def update_colors(self, freq_amps):
 
         dt = time.time() - self.sig_time_history[-1]
         self.sig_time_history.append(time.time())
 
-        rgb_raw = self._choose_rgb_signals(freq_amps, dt)
+        rgb_raw = self._choose_rgb_signals(freq_amps)
+        self._update_signal_intensity_multipliers()
 
         self.signal_intensity_maxes = [max(c,m) for c, m in zip(rgb_raw, self.signal_intensity_maxes)]
-        r, g, b = [x/m for x, m in zip(rgb_raw, self.signal_intensity_maxes)]
+        r, g, b = [x/(m*mult) for x, m, mult in zip(rgb_raw, self.signal_intensity_maxes, self.rgb_max_mults)]
         smooth_r, smooth_g, smooth_b = self._smooth(r, g, b)
         #print(smooth_r, smooth_g, smooth_b)
 
         excited_r = max(smooth_r, r)
+        self.rv = ColorVisualizer.GRAVITY + ColorVisualizer.ACCELERATION * excited_r 
+        self.gv = ColorVisualizer.GRAVITY + ColorVisualizer.ACCELERATION * smooth_g 
+        self.bv = ColorVisualizer.GRAVITY + ColorVisualizer.ACCELERATION * smooth_b 
 
-        #print(Color.ACCELERATION * smooth_g)
-        r_fall_const = 1#max(self.signal_intensity_maxes[0]/ColorVisualizer.R_CONST, 1)
-        g_fall_const = 1#max(self.signal_intensity_maxes[1]/ColorVisualizer.G_CONST, 1)
-        b_fall_const = 1#max(self.signal_intensity_maxes[2]/ColorVisualizer.B_CONST, 1)
-
-        print(r_fall_const, g_fall_const, b_fall_const)
-
-        self.rv = ColorVisualizer.GRAVITY * r_fall_const + ColorVisualizer.ACCELERATION * excited_r 
-        self.gv = ColorVisualizer.GRAVITY * g_fall_const + ColorVisualizer.ACCELERATION * smooth_g 
-        self.bv = ColorVisualizer.GRAVITY * b_fall_const + ColorVisualizer.ACCELERATION * smooth_b 
+       # print(self.rgb_coeff)
 
         for i, m_i in enumerate(self.signal_intensity_maxes):
             if m_i > ColorVisualizer.SIGNAL_INTENSITY_DRAG:
                 drag = ColorVisualizer.SIGNAL_INTENSITY_DRAG 
                 if m_i > ColorVisualizer.SIGNAL_INTENSITY_THRESHOLD:
-                    drag *= ColorVisualizer.RGB_CONST[i]
+                    drag *= ColorVisualizer.INIT_RGB_COEFF[i]
                 self.signal_intensity_maxes[i] = max(ColorVisualizer.SIGNAL_INTENSITY_MAX_START, m_i - drag)
-
 
         self.red += self.rv 
         self.green += self.gv
         self.blue += self.bv
 
         self.red, self.green, self.blue = self._bounds_check(self.red, self.green, self.blue)
+        rgb = [self.red, self.green, self.blue]
+        for c, c_deque in zip(rgb, self.rgb_history):
+            c_deque.append(c)
+
