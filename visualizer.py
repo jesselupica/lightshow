e671614c52a263801a925b21a55c473395be9cb7
jesselupica <jesselupica@gmail.com>
2016-12-17 15:22:18 -0700

    added requirements file

--- visualizer.py
+++ visualizer.py
@@ -1,17 +1,7 @@
 from collections import deque
 import time
-import random
 
 class ColorVisualizer: 
-    # acc 10 gravity -4 works well
-
-    # gooey
-    # ACCELERATION = 10
-    # G_CONST = 2
-    # B_CONST = 1
-    # R_CONST = 4
-    # GRAVITY = -4
-
     ACCELERATION = 12.5
     INIT_RGB_COEFF = 12, 20, 16
     GRAVITY = -6
@@ -24,6 +14,9 @@ class ColorVisualizer:
     
     SIGNAL_INTENSITY_DRAG = 5
     SIGNAL_INTENSITY_GAIN = 30
+
+    HIT_CONST = 60
+    HIT_COEFF = 3
 
     def __init__(self, r, g, b, update_freq):
         r, g, b = self._bounds_check(r, g, b)
@@ -45,7 +38,6 @@ class ColorVisualizer:
         self.rgb_raw_signal_history = [deque([0], maxlen=5)] * 3
 
     def tuple(self):
-        #print(str(self.red) + ', ' + str(self.green) + ', ' + str(self.blue))
         return (self.red, self.green, self.blue)
 
     def _bounds_check(self, r, g, b):
@@ -70,20 +62,15 @@ class ColorVisualizer:
         return r_sum/l, g_sum/l, b_sum/l
 
     def _choose_rgb_signals(self, freq_amps):
-        # for i, m_i in enumerate(max_intensities):
-        #    if m_i > intensity_drag:
-        #        max_intensities[i] = max(300, m_i - intensity_drag)
         # 3 for RGB
         assert(len(freq_amps) >= 3)
         freq_amp_pairs = list(enumerate(freq_amps))
         sorted_fa_pairs = sorted(freq_amp_pairs, key=lambda x: x[1])
         rgb = sorted(sorted_fa_pairs[-3:], key=lambda x: x[0])
-        #print([ int(2**((i-49)/12) * 440) for i, a in rgb])
         return [x[1] for x in rgb], [x[0] for x in rgb]
 
     def _update_signal_intensity_multipliers(self):
         avg_rgb = [sum(d)/len(d) for d in self.rgb_history]
-        #print(avg_rgb, len(self.rgb_history[0]))
         for i, avg_c in enumerate(avg_rgb):
             if avg_c > ColorVisualizer.RGB_INTENSITY_THRESHOLD:
                 self.rgb_max_mults[i] *= ColorVisualizer.RGB_COEFF_INCR
@@ -91,11 +78,10 @@ class ColorVisualizer:
                 self.rgb_max_mults[i] /= ColorVisualizer.RGB_COEFF_INCR 
 
     def _is_hit(self, rgb_raw):
-        hit_cooef = 3
         avg_signal_amp = [sum(d)/len(d) for d in self.rgb_raw_signal_history]
         for sig, deq in zip(rgb_raw, self.rgb_raw_signal_history):
             deq.append(sig) 
-        return tuple([sig > hit_cooef * avg for sig, avg in zip(rgb_raw, avg_signal_amp)])
+        return tuple([sig > ColorVisualizer.HIT_COEFF * avg for sig, avg in zip(rgb_raw, avg_signal_amp)])
 
     def update_colors(self, freq_amps):
 
@@ -104,13 +90,12 @@ class ColorVisualizer:
 
         rgb_raw, rgb_ints = self._choose_rgb_signals(freq_amps)
         is_hit = self._is_hit(rgb_raw)
-        hit_const = [80 if h else 0 for h in is_hit]
+        hit_const = [ColorVisualizer.HIT_CONST if h else 0 for h in is_hit]
 
         self._update_signal_intensity_multipliers()
 
         self.signal_intensity_maxes = [max(c,m) for c, m in zip(rgb_raw, self.signal_intensity_maxes)]
         r, g, b = [x/(m*mult) for x, m, mult in zip(rgb_raw, self.signal_intensity_maxes, self.rgb_max_mults)]
-        #print( str(round(r, 5)) + '   \t' + str(round(g, 5)) + '   \t' + str(round(b, 5)))
         smooth_r, smooth_g, smooth_b = self._smooth(r, g, b)
 
         excited_r = max(smooth_r, r)
